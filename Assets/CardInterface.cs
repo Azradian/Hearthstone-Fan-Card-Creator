8dbdc37599e44b8ec2abb65f2707e7ebc00b0867
Azradian <Azradian77@gmail.com>
2021-04-13 03:41:45 -0400

    Finally getting around to commiting my Hearthstone Card Creator Tool!

---
+++ Assets/CardInterface.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardInterface : MonoBehaviour
+{
+    public Card card;
+
+    public TMP_Text nameText;
+    public TMP_Text descriptionText;
+
+    public Image artImage;
+
+    public TMP_Text manaCostText;
+    public TMP_Text attackText;
+    public TMP_Text healthText;
+
+
+    void Start()
+    {
+        // Let's create our card!
+
+        /*
+        nameText.text = card.cardName;
+        descriptionText.text = card.cardDescription;
+
+        artImage.sprite = card.cardArt;
+
+        manaCostText.text = card.manaCost.ToString();
+        attackText.text = card.attack.ToString();
+        healthText.text = card.health.ToString();
+        */
+
+        nameText.SetText(card.cardName);
+        descriptionText.SetText(card.cardDescription);
+
+        artImage.sprite = card.cardArt;
+
+        manaCostText.SetText(card.manaCost.ToString());
+        attackText.SetText(card.attack.ToString());
+        healthText.SetText(card.health.ToString());
+    }
+
+
+}
---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Let's create a scriptable object that can be used to easily create more cards!
+
+[CreateAssetMenu]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public string cardDescription;
+
+    public Sprite cardArt;
+
+    public int manaCost;
+    public int attack;
+    public int health;
+
+    public void PrintCardInfo()
+    {
+        Debug.Log(cardName + ": " + cardDescription + " The card costs: " + manaCost);
+    }
+}
